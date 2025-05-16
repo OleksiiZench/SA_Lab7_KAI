36222657d99714400e34074ce4e48c868e7b0869
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-05-16 17:01:53 +0300

    Налаштували mapping BLL/ API

---
+++ FoodDelivery.API/Mapping/ApiMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using FoodDelivery.API.Models;
+using FoodDelivery.BLL.Models;
+
+namespace FoodDelivery.API.Mapping
+{
+    public class ApiMappingProfile : Profile
+    {
+        public ApiMappingProfile()
+        {
+            CreateMap<CategoryDto, CategoryModel>().ReverseMap();
+            CreateMap<DishDto, DishModel>().ReverseMap();
+            CreateMap<DayOfWeekDto, DayOfWeekModel>().ReverseMap();
+            CreateMap<MenuDto, MenuModel>().ReverseMap();
+            CreateMap<OrderDto, OrderModel>().ReverseMap();
+            CreateMap<OrderItemDto, OrderItemModel>().ReverseMap();
+            CreateMap<CreateOrderItemModel, OrderItemDto>();
+        }
+    }
+}
